b9865b4ef05ff9bfa92411556c2987365440c168
dse <dse@ds-s.at>
2022-10-14 11:23:33 +0200

    Initial commit

---
+++ src/main/java/com/example/slm_last_turn/SlmLastTurnApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_last_turn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmLastTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmLastTurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slm_last_turn/SlmLastTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_last_turn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmLastTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
